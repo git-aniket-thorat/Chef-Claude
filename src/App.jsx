de56ce5627750fd957f2e0fddc5ae375ad25b612
Aniket Thorat <thorataniket088@gmail.com>
2026-01-18 18:43:29 +0530

    This is the commit to for the header of the page
    THe component has img and and h1 tag which has chef-logo and Chef Claude header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import Headers from './components/Header.jsx'
 
 function App() {
 
 
   return (
     <>
-      
+      <Headers/>
     </>
   )
 }
